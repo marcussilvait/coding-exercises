bae8f59af5bc0e008f36232e908a747fabba7424
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-05-07 21:57:49 -0300

    Added class Client and getters and setters

--- ooalura/bank.py
+++ ooalura/bank.py
@@ -1,9 +1,9 @@
 class Account:
-    def __init__(self, number, holder, balance, overdraft_limits):
+    def __init__(self, number, holder, balance, overdraft_limit):
         self.__number = number
         self.__holder = holder
         self.__balance = balance
-        self.__overdraft_limits = overdraft_limits
+        self.__overdraft_limit = overdraft_limit
 
     def statement(self):
         print(f'{self.__holder}, you have ${self.__balance} Available')
@@ -17,3 +17,38 @@ class Account:
     def transfer(self, valor, allocation):
         self.withdraw(valor)
         allocation.deposit(valor)
+
+    @property
+    def number(self):
+        return self.__number
+
+    @property
+    def holder(self):
+        return self.__holder
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def overdraft_limit(self):
+        return self.__overdraft_limit
+
+    @overdraft_limit.setter
+    def overdraft_limit(self, overdraft_limit):
+        self.__overdraft_limit = overdraft_limit
+
+
+class Client:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        print('calling @property name()')
+        return self.__name.title()
+
+    @name.setter
+    def name(self, name):
+        print('calling setter name()')
+        self.__name = name
