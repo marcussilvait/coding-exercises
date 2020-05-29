748dd2b197885f24eb60c080b3055eccbf15bc48
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-05-29 10:46:09 -0300

    Atualização de teste utilizando pytest-mock

--- pythonpro/test_github_api.py
+++ pythonpro/test_github_api.py
@@ -1,26 +1,30 @@
 from unittest.mock import Mock
 
+import pytest
+
 from pythonpro import github_api
 
 
-# Teste unitário
-def test_buscar_avatar_unitario():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars1.githubusercontent.com/u/45732834?v=4'
     resp_mock.json.return_value = {
-        'login': 'marvinsilva',
-        'id': 45732834,
-        'node_id': 'MDQ6VXNlcjQ1NzMyODM0',
-        'avatar_ur': 'https://avatars1.githubusercontent.com/u/45732834?v=4',
-        'url': 'https://api.github.com/users/marvinsilva',
-        'html_url': 'https://github.com/marvinsilva',
-        'followers_url': 'https://api.github.com/users/marvinsilva/followers',
+        'login': 'marcus', 'id': 3090, 'node_id': 'MDQ6VXNlcjMwOTA=',
+        'avatar_url': url,
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('pythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+# Teste unitário
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('marvinsilva')
-    assert 'https://avatars1.githubusercontent.com/u/45732834?v=4' == url
+    assert avatar_url == url
 
 
-# Teste de integração dependente da rede e github server
+# Teste de integração
 def test_buscar_avatar_integracao():
-    url = github_api.buscar_avatar('marvinsilva')
-    assert 'https://avatars1.githubusercontent.com/u/45732834?v=4' == url
+    url = github_api.buscar_avatar('marcus')
+    assert 'https://avatars2.githubusercontent.com/u/3090?v=4' == url
