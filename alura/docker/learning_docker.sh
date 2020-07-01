a12f1ff508b2bfe32dbff7e08fc01c8f961d3c64
Marcus Vinicius Laurindo da Silva <45732834+marvinsilva@users.noreply.github.com>
2020-06-30 22:29:17 -0300

    Update learning_docker.sh

--- alura/docker/learning_docker.sh
+++ alura/docker/learning_docker.sh
@@ -150,3 +150,5 @@ docker network create --driver bridge NOME_DA_REDE # cria uma rede especificando
 docker-compose build # Realiza o build dos serviços relacionados ao arquivo docker-compose.yml, assim como verifica a sua sintaxe.
 docker-compose up # Sobe todos os containers relacionados ao docker-compose, desde que o build já tenha sido executado.
 docker-compose down # Para todos os serviços em execução que estejam relacionados ao arquivo docker-compose.yml.
+docker-compose run web python -V # Executa o comando python version no container web
+docker-compose run web pipenv install django==2.2.9 # Instala o django na versão 2.2.9 no container web
